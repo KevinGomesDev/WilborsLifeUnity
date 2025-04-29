6f461f1a4638ff20232c611252be9c1cce031d7f
KevinGomesDev <kevingomesdev2025@gmail.com>
2025-04-29 13:57:25 -0300

    initial commit

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Player player;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        player = GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (player.direction.sqrMagnitude > 0)
+        {
+            anim.SetInteger("Transition", 1);
+        } else
+        {
+            anim.SetInteger("Transition", 0);
+        }
+    }
+}
